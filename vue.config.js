2bf2448173330ae513eccb5a5449f1e311f666de
wenblack <wenblack012@gmail.com>
2022-03-21 03:43:46 -0300

    Teste

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? 
-    '/churras-maker' : '/'
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/churras-maker/dist" : "/",
 });
